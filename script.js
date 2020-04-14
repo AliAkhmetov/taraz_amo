ab25681e15f9a8428eb695f05eb35133e558c1d6
AliAkhmetov <AliAkhmetov@users.noreply.github.com>
2020-04-14 13:26:40 +0300

    done
    done

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 function changeColor() {
-  var room1 = document.getElementById('room1')
-  var room2 = document.getElementById('room2')
-  var flag = true;
+                var buttonTipe = this;
+                    console.log(buttonTipe);
+                var buttonClass = buttonTipe.classList;
+                    console.log(buttonClass);
+var c1;
+if (buttonClass.contains("button1")) {
+    c1 = 'circle1';
+} else {
+    c1 = 'circle2';
+}
 
-  if (flag == true) {
-    room1.classList.add("light");
-    var colorArray = document.getElementsByClassName ("room1")
-    flag = false;
-    }
-    else {
-      room2.classList.add("light")
-      room1.classList.remove("light")
-        flag = true;
-    }
+var circle = document.getElementsByClassName(c1);
+
+for(var i in circle){
+  var  circleClass = circle[i].classList;
+  if(!circleClass)
+    continue;
+  if (circleClass.contains("light")) {
+    circleClass.remove("light");
+  } else {
+      circleClass.add("light");
   }
+}
+}
